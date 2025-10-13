5e39812c0085b1c629e267fef7bc9c21f02ca283
DariusCotrau <87205413+DariusCotrau@users.noreply.github.com>
2025-10-13 09:31:02 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book("Design Patterns in Java");
-        book.addAuthor("Gamma");
-        Chapter ch1 = book.addChapter("Chapter 1");
-        SubChapter sc1 = ch1.addSubChapter("Intro");
-        sc1.addParagraph("First paragraph");
-        sc1.addImage("diagram.png");
-        sc1.addTable("Table 1.1");
-
-        // Demonstrăm print-ul moștenit
-        book.print();
-        ch1.print();
-        sc1.print();
-        for (TableOfContents el : sc1.getElements()) {
-            el.print();
-        }
-        // și accesul la asocierea Book-TOC
-        book.getTableOfContents().print();
+        Book noapteBuna = new Book("Noapte buna, copii!");
+        Author rpGheo = new Author("Radu Pavel Gheo");
+        noapteBuna.addAuthor(rpGheo);
+        Section cap1 = new Section("Capitolul 1");
+        Section cap11 = new Section("Capitolul 1.1");
+        Section cap111 = new Section("Capitolul 1.1.1");
+        Section cap1111 = new Section("Subchapter 1.1.1.1");
+        noapteBuna.addContent(new Paragraph("Multumesc celor care ..."));
+        noapteBuna.addContent(cap1);
+        cap1.add(new Paragraph("Moto capitol"));
+        cap1.add(cap11);
+        cap11.add(new Paragraph("Text from subchapter 1.1"));
+        cap11.add(cap111);
+        cap111.add(new Paragraph("Text from subchapter 1.1.1"));
+        cap111.add(cap1111);
+        cap1111.add(new Image("Image subchapter 1.1.1.1"));
+        noapteBuna.print();
     }
 }
 
